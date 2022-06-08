5541ae40aea7e7506da1b01904364e9983491ecf
Gerald <Tu correo>
2022-06-08 17:50:31 -0500

    Implementando frontend

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule )
+  },
+  {
+    path: 'admin-dashboard',
+    loadChildren: () => import('./admin/admin.module').then( m => m.AdminModule )
+  },
+  {
+    path: '**',
+    redirectTo: 'auth'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/auth/pages/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styles: [],
+})
+export class LoginComponent {
+  constructor(private router: Router) {}
+
+  login() {
+    this.router.navigateByUrl('/admin-dashboard');
+  }
+}
